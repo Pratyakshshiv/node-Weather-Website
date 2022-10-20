4dd8a4b56e1d4a5fad9baadc6f9792d8b4ea91a1
pratyaksh <pratyakshshivhare@gmail.com>
2022-10-20 18:04:54 +0530

    update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=53a72ecfa950a48e9c30b38df4e65701&query=' + latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=53a72ecfa950a48e9c30b38df4e65701&query=' + latitude + ',' + longitude + '&units=m'
 
 
     request({ url, json: true }, (error, { body }) => {
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('unable to find location', undefined)
         }
         else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out.It feels like ' + body.current.feelslike + ' degrees out.The humidity is ' + body.current.humidity + '%.')
+            callback(undefined, body.current.weather_descriptions[0] + '.It is currently ' + body.current.temperature + '°C out.It feels like ' + body.current.feelslike + ' degrees out.The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
